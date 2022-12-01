209a3178d5d088949a06e13fb9ed38d6ca880413
Ai Xia <ayx201@nyu.edu>
2022-11-30 16:20:27 -0800

    more game stuff

--- public/sketch.js
+++ public/sketch.js
@@ -5,6 +5,10 @@ let pace;
 let coherenceScore;
 let collectedCoin;
 let img;
+let MENU = 0
+let SPbutton, MPbutton;
+let catMC;
+let setupTrigger = true
 //Open and connect socket
 let socket = io();
 
@@ -18,11 +22,9 @@ function preload() {
 }
 
 function setup() {
-  
   createCanvas(800, 600);
 
-
-  gameOneSetup();
+  // catMC = loadAni('catWalkRight',4)
 
   //Listen for messages named 'data' from the server
   socket.on('data', function (obj) {
@@ -34,26 +36,36 @@ function setup() {
   slider.position(width / 2.2, 860);
   slider.style("width", "150px");
 
-  //Gems setup
-  // gems = new Group();
-  // gems.diameter = 10;
-  // gems.x = () => random(0, width);
-  // gems.y = () => random(0, height);
-  // gems.amount = 80;
 
+  // if (MENU == 1) {
+  //   player = new Sprite(width / 10, height / 2, 50);
 
-  // player.overlaps(gems, collect);
+  // coin = new Sprite((width / 10) * 9, height / 2, 30);
+
+  // }
+  // if (MENU == 2) {
+  //   }
+
 
 }
 
 function draw() {
 
-  
-  // preload();
+  startMenu();
 
-  //PACER LINE
-  pace = slider.value();
-  pacerLine();
+  if (MENU == 1 && setupTrigger == true) {
+    gameOneSetup();
+      setupTrigger = false
+  }
+
+  if (MENU == 1){
+    gameOne();
+  }
+
+  if (MENU == 2) {
+  }
+
+  // preload();
 
   //Coherence Score from Sockets.io
   socket.on('newdata', function (score) {
@@ -64,21 +76,21 @@ function draw() {
   })
 
   //startScreen();
-  gameOne();
-
 
 
 }
 
-function startScreen(){
-background(230);
-//Check if emWave pro is connected
-}
 
 //GAME ONE - HOW TO INCREASE COHERENCE
 function gameOne() {
-background(150);
+
+  background(150);
   image(img, 0, 0, width, height, 0, 0, img.width, img.height);
+
+  //SLIDER AND BREATH PACER
+  pace = slider.value();
+  pacerLine();
+
   //DISPLAY COHERENCE SCORE
   noStroke();
   textSize(32);
@@ -103,24 +115,33 @@ background(150);
 
   if (player.overlaps(coin)) {
     coin.remove();
-  }  
+  }
 
 }
 
-function gameOneSetup(){
+function gameOneSetup() {
+  console.log(MENU)
   player = new Sprite(width / 10, height / 2, 50);
+  // player.addAni('catWalkRight',4)
+  // player.addAni('walkRight','catWalkRight/tile1.png',4)
 
   coin = new Sprite((width / 10) * 9, height / 2, 30);
 }
 
 //GAME TWO - COHERENCE IN STRESSFUL SITUATIONS
 
-function gameTwoSetup(){
-
+function gameTwoSetup() {
+  //Gems setup
+  // gems = new Group();
+  // gems.diameter = 10;
+  // gems.x = () => random(0, width);
+  // gems.y = () => random(0, height);
+  // gems.amount = 80;
+  // player.overlaps(gems, collect);
 }
 
-function gameTwo(){
-    //Keyboard controls
+function gameTwo() {
+  //Keyboard controls
   // if (kb.pressing('up')) {
   //   player.direction = -90;
   // } else if (kb.pressing('down')) {
@@ -142,6 +163,49 @@ function gameTwo(){
 // function collect(player, gem) {
 //   gem.remove();
 // }
+
+function startMenu() {
+  image(img, 0, 0, width, height, 0, 0, img.width, img.height);
+  print(mouseX, mouseY)
+  textSize(26);
+  text('lets play!', width / 2, height / 2)
+
+  let leftAlign = width - 200
+
+  //SP BUTTON
+  fill(0, 255, 0);
+  rect(leftAlign - 20, 50, 200, 75);
+  fill(255);
+  text('Single Player', leftAlign, 106);
+
+  //MP BUTTON
+  fill(255, 0, 255);
+  rect(leftAlign - 20, 200, 200, 75);
+  fill(255);
+  text('MultiPlayer', leftAlign, 248);
+
+  // //INSTRUCTIONS BUTTON
+  // text('Instructions', 94, 406);
+  // fill(255, 0, 0);
+  // rect(50, 350, 200, 75);
+
+}
+
+function mouseClicked() {
+  if (MENU == 0) {
+    if (mouseX < 780 && mouseX > 577) {
+      if (mouseY < 125 && mouseY > 50) {
+        MENU = 1
+      }
+      if (mouseY < 275 && mouseY > 200) {
+        MENU = 2
+      }
+      if (mouseY < 425 && mouseY > 350) {
+        MENU = 3
+      }
+    }
+  }
+}
 
 function pacerLine() {
   let r = map(sin(angle), 1, -1, width / 4, (width / 4) * 3);
